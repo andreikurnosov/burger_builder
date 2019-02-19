c17729285ed73b5b8179cd5ece32dbdec6e11d38
nixdie <nixdie@yandex.ru>
2019-02-19 15:49:34 +0700

    hamburger icon, changed folder structure

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,31 +1,40 @@
-import React from 'react';
-import Aux from '../../../hoc/Aux';
+import React, {Component} from 'react';
+import Aux from '../../../hoc/Aux/Aux';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-  const ingredientSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component {
+
+  componentDidUpdate() {
+    console.log('[OrderSummary] WillUpdate')
+  }
+
+  render () {
+    const ingredientSummary = Object.keys(this.props.ingredients)
     .map(igKey => {
       return (
         <li key={igKey + 1}>
-          <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
+          <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}
         </li>
       )
     })
 
-  return (
-    <Aux>
-      <h3>Your Order</h3>
-      <p>A delicious burger with the following ingredients:</p>
-      <ul>
-        {ingredientSummary}
-      </ul>
-      <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-      <p>Continue to Checkout?</p>
-      <Button clicked={props.purchaseCancelled} btnType="Danger">CANCEL</Button>
-      <Button clicked={props.purchaseContinued} btnType="Success">CONTINUE</Button>
+    return (
+      <Aux>
+        <h3>Your Order</h3>
+        <p>A delicious burger with the following ingredients:</p>
+        <ul>
+          {ingredientSummary}
+        </ul>
+        <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+        <p>Continue to Checkout?</p>
+        <Button clicked={this.props.purchaseCancelled} btnType="Danger">CANCEL</Button>
+        <Button clicked={this.props.purchaseContinued} btnType="Success">CONTINUE</Button>
 
-    </Aux>
-  )
-};
+      </Aux>
 
-export default orderSummary;
+    )
+  }
+}
+
+
+export default OrderSummary;
