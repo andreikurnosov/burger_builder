d6e78ff7223db5d330dd61c87adaf8859f051319
nixdie <nixdie@yandex.ru>
2019-02-25 11:08:03 +0700

    added auth form

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
 import Orders from './containers/Orders/Orders';
+import Auth from './containers/Auth/Auth'
 
 class App extends Component {
   // componentDidMount() {
@@ -20,7 +21,9 @@ class App extends Component {
           <Switch>
             <Route path="/checkout" component={Checkout}/>
             <Route path="/orders" component={Orders}/>
+            <Route path="/auth" exact component={Auth}/>
             <Route path="/" exact component={BurgerBuilder}/>
+
           </Switch>
         </Layout>
 
