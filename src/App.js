128984ed1c7225d38508c31326bde275336136d1
nixdie <nixdie@yandex.ru>
2019-02-25 14:23:04 +0700

    added logout link

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
 import Orders from './containers/Orders/Orders';
-import Auth from './containers/Auth/Auth'
+import Auth from './containers/Auth/Auth';
+import Logout from './containers/Auth/Logout/Logout';
+
 
 class App extends Component {
   // componentDidMount() {
@@ -22,6 +24,7 @@ class App extends Component {
             <Route path="/checkout" component={Checkout}/>
             <Route path="/orders" component={Orders}/>
             <Route path="/auth" exact component={Auth}/>
+            <Route path="/logout" exact component={Logout}/>
             <Route path="/" exact component={BurgerBuilder}/>
 
           </Switch>
